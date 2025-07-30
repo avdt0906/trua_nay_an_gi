9e1219ecc983aea3abfa7ff69e4d5b7604c3e87b
Kadb96 <ptvu.2838@gmail.com>
2025-07-30 21:31:54 +0700

    create entity for database

---
+++ src/main/java/com/codegym/project_module_5/model/Dish.java
@@ -0,0 +1,32 @@
+package com.codegym.project_module_5.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dish {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+    private Double price;
+    private String description;
+    private String picture_url;
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+    private Boolean is_available = true;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
--- src/main/java/com/codegym/project_module_5/model/OrderStatus.java
+++ src/main/java/com/codegym/project_module_5/model/OrderStatus.java
@@ -1,6 +1,9 @@
 package com.codegym.project_module_5.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,17 +14,9 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class OrderStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String password;
-    private String email;
-    private String phone;
-    private String fullName;
-    private String avatar_url;
-    @ManyToOne
-    @JoinColumn(name = "role_id")
-    private Role role;
+    private String name;
 }
