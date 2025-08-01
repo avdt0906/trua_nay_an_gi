6e11d73ffc0270f61b8df2f7c147097c9bf3ced1
Kadb96 <ptvu.2838@gmail.com>
2025-08-01 20:49:58 +0700

    tinh nang login

---
+++ src/main/java/com/codegym/project_module_5/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.codegym.project_module_5.service.impl;
+
+import com.codegym.project_module_5.model.User;
+import com.codegym.project_module_5.repository.UserRepository;
+import com.codegym.project_module_5.model.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
