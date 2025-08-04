1e8dd571cadef61a86a47c1f498cea5046932e4a
taobitk <8hungvip8@gmail.com>
2025-08-04 21:04:38 +0700

    admin view

--- src/main/java/com/codegym/project_module_5/controller/admin/AdminController.java
+++ src/main/java/com/codegym/project_module_5/controller/admin/AdminController.java
@@ -1,4 +1,26 @@
 package com.codegym.project_module_5.controller.admin;
 
+import com.codegym.project_module_5.model.User;
+import com.codegym.project_module_5.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping("/dashboard")
+    public String showAdminDashboard(Model model) {
+        List<User> owners = userService.findAllByRoleName("OWNER");
+        model.addAttribute("owners", owners);
+        return "admin/dashboard";
+    }
 }
--- src/main/java/com/codegym/project_module_5/service/impl/UserService.java
+++ src/main/java/com/codegym/project_module_5/service/impl/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -65,5 +66,9 @@ public class UserService implements IUserService {
     @Override
     public void save(User user) {
         userRepository.save(user);
+    }
+    @Override
+    public List<User> findAllByRoleName(String roleName) {
+        return userRepository.findAllByRole_Name(roleName);
     }
 }
