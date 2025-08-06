34ce9dae240304e2fd1cff181f984756a49d3ba4
taobitk <8hungvip8@gmail.com>
2025-08-06 20:26:40 +0700

    creat view admin

---
+++ src/main/java/com/codegym/project_module_5/repository/IOrderRepository.java
@@ -0,0 +1,9 @@
+package com.codegym.project_module_5.repository;
+
+import com.codegym.project_module_5.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IOrderRepository extends JpaRepository<Order, Long> {
+}
