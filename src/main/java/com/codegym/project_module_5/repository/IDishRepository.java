ea00a3f537d939854064e71b67c02cfb7b5a8b1f
Kadb96 <ptvu.2838@gmail.com>
2025-08-13 20:18:40 +0700

    merge code

--- src/main/java/com/codegym/project_module_5/repository/IDishRepository.java
+++ src/main/java/com/codegym/project_module_5/repository/IDishRepository.java
@@ -1,7 +1,7 @@
 package com.codegym.project_module_5.repository;
 
 import com.codegym.project_module_5.model.Dish;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 
 public interface IDishRepository extends CrudRepository<Dish, Long> {
