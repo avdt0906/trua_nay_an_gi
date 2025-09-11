b969acbaf23b5fbe95651563b22544b33cc5badb
khanh-desgin <buiduykhanh1062003@gmail.com>
2025-09-11 10:22:42 +0700

    create evalaute successfull

--- src/main/java/com/codegym/project_module_5/controller/client/restaurant_client/RestaurantClientController.java
+++ src/main/java/com/codegym/project_module_5/controller/client/restaurant_client/RestaurantClientController.java
@@ -1,9 +1,11 @@
 package com.codegym.project_module_5.controller.client.restaurant_client;
 
+import com.codegym.project_module_5.model.evaluate_model.Evaluate;
 import com.codegym.project_module_5.model.restaurant_model.Category;
 import com.codegym.project_module_5.model.restaurant_model.Dish;
 import com.codegym.project_module_5.model.restaurant_model.Restaurant;
 import com.codegym.project_module_5.repository.restaurant_repository.ICategoryRepository;
+import com.codegym.project_module_5.service.impl.evaluate_service_impl.EvaluateService;
 import com.codegym.project_module_5.service.impl.restaurant_service_impl.CategoryService;
 import com.codegym.project_module_5.service.impl.restaurant_service_impl.DishService;
 import com.codegym.project_module_5.service.impl.restaurant_service_impl.RestaurantService;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,11 +32,15 @@ public class RestaurantClientController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private EvaluateService evaluateService;
+
     @GetMapping("/{id}")
     public String showRestaurantDetails(@PathVariable Long id, Model model) {
         List<Dish> dishes = dishService.findByRestaurantId(id);
         Optional<Restaurant> restaurant = restaurantService.findById(id);
         Iterable<Category> categories = categoryService.findAll();
+        Iterable<Evaluate> evaluates = evaluateService.findAll();
 
         if (!dishes.isEmpty()) {
             double minPrice = dishes.stream().mapToDouble(Dish::getPrice).min().orElse(0);
@@ -44,6 +51,7 @@ public class RestaurantClientController {
             model.addAttribute("priceRange", "Đang cập nhật");
         }
 
+        model.addAttribute("evaluate", evaluates);
         model.addAttribute("restaurant", restaurant.get());
         model.addAttribute("categories", categories);
         model.addAttribute("dishes", dishes);
