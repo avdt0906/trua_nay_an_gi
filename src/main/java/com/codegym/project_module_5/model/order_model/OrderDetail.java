a2a22da3abaa1c025a53474f976af020e07e1943
taobitk <8hungvip8@gmail.com>
2025-09-15 20:00:19 +0700

    da xong dat hang con xem lich su

--- src/main/java/com/codegym/project_module_5/model/order_model/OrderDetail.java
+++ src/main/java/com/codegym/project_module_5/model/order_model/OrderDetail.java
@@ -12,16 +12,22 @@ import lombok.*;
 @AllArgsConstructor
 public class OrderDetail {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "order_id")
     @NotNull
     private Orders order;
+
     @ManyToOne
     @JoinColumn(name = "dish_id")
     @NotNull
     private Dish dish;
+
     @NotNull
     private Long quantity;
+
+    @NotNull
+    private Double price;
 }
--- src/main/java/com/codegym/project_module_5/repository/order_repository/IOrderRepository.java
+++ src/main/java/com/codegym/project_module_5/repository/order_repository/IOrderRepository.java
@@ -1,10 +1,15 @@
 package com.codegym.project_module_5.repository.order_repository;
 
+import com.codegym.project_module_5.model.order_model.OrderStatus;
 import com.codegym.project_module_5.model.order_model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IOrderRepository extends JpaRepository<Orders, Long> {
     Iterable<Orders> findAllByRestaurantId(Long restaurantId);
+
+
 }
