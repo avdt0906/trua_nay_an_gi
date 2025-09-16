dc8a4cf54c5df6d601b387f375f1600e5fdd85bc
khanh-desgin <buiduykhanh1062003@gmail.com>
2025-09-16 07:34:37 +0700

    create order statistics successfull

--- src/main/java/com/codegym/project_module_5/repository/order_repository/IOrderRepository.java
+++ src/main/java/com/codegym/project_module_5/repository/order_repository/IOrderRepository.java
@@ -1,10 +1,14 @@
 package com.codegym.project_module_5.repository.order_repository;
 
+import com.codegym.project_module_5.model.order_model.OrderDetail;
 import com.codegym.project_module_5.model.order_model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IOrderRepository extends JpaRepository<Orders, Long> {
     Iterable<Orders> findAllByRestaurantId(Long restaurantId);
+//    List<Orders> findAllByOrderId(Long orderId, Long userId);
 }
